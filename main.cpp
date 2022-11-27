9882842dd797929a96fe8326c2f23ad84063b358
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-11-27 02:50:29 +0000

    Update main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <cstdlib>
 #include <iostream>
 #include <time.h>
+#include <array>
 using namespace std;
 
-bool players_bets[5] = {true, true, true, false, true}; //Update this as bets are added
-
-int roll_dice() {
-  int roll1 = rand() % 6 + 1;
-  int roll2 = rand() % 6 + 1;
-  return roll1 + roll2;
-}
-
-void print_players_bets(){
-  int bets_array_length = sizeof(players_bets) / sizeof(players_bets[0]);
-  for (int i = 0; i < bets_array_length; i++){
-    std::cout << players_bets[i] << " ";
-  }
-  std:cout <<"\n";
-}
-
+bool players_bets[5] = {true, false, false, false, false}; //Update this as bets are added
+/*
+ ***BET KEY***
+index 0: Field
+index 1: n/a
+index 2: n/a
+index 3: n/a
+index 4: n/a
+*/
 void setBet(int bet_index){
   players_bets[bet_index] = true;
 }
@@ -27,15 +21,44 @@ void clearBet(int bet_index){
   players_bets[bet_index] = false;
 }
 
+bool is_field_roll(int dice_roll){
+  if (dice_roll == 2 || dice_roll == 3 || dice_roll == 4 || dice_roll == 9 || dice_roll == 10 || dice_roll == 11 || dice_roll == 12){
+    return true;
+  }
+  else return false;
+}
+
+int roll_dice() {
+  int roll1 = rand() % 6 + 1;
+  int roll2 = rand() % 6 + 1;
+  return roll1 + roll2;
+}
+
+void print_bets(bool array[]){
+  int array_length = sizeof(array) / sizeof(array[0]);
+  for (int i = 0; i < array_length; i++){
+    std::cout << array[i] << " ";
+  }
+  std:cout <<"\n";
+}
+
+std::array <bool,5> check_roll(bool array[], int players_dice_roll){
+  bool winning_bets[5] = {false};
+  if (array[0] == true){
+    if (is_field_roll(players_dice_roll)){ 
+      winning_bets[0] = true;
+      }
+  }
+  
+  return winning_bets[];
+}
+
 
  /* MAIN FUNCTION BELOW */
 int main(){
   srand(time(NULL));
-  print_players_bets();
-  setBet(3);
-  print_players_bets();
-  clearBet(0);
-  print_players_bets();
+  int players_roll = roll_dice();
+  print_bets(check_roll(players_bets, players_roll));
 
   /*
   int roll{};
