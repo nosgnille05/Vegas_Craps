3531f43e00b9e78e8fd5295e464485ddaf51beb8
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-11-30 03:58:28 +0000

    Update main

--- .ccls-cache/@home@runner@VegasCraps/main.cpp
+++ .ccls-cache/@home@runner@VegasCraps/main.cpp
@@ -8,8 +8,11 @@
 #include <cstring>
 using namespace std;
 const int NUM_BETS = 5;
-double players_bets[5][2] = {{1, 10}, {1, 10}, {1, 10}, {0, 0}, {0, 0}}; //Update to test players bets
-double winning_bets[NUM_BETS][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+double players_bank = 10;
+double players_bet_amount{};
+double players_bets[5][2] = {{1, players_bet_amount}, {1, players_bet_amount}, {1, players_bet_amount}, {0, players_bet_amount}, {0, players_bet_amount}}; //Update to test players bets
+double winning_bets[NUM_BETS][2] = {{0, 2}, {0, 3}, {0, 4}, {0, 0}, {0, 0}};
+
 /*
  ***BET KEY***
 index 0: Basic Field (3,4,9,10,11)
@@ -18,6 +21,21 @@ index 2: Twelve Field (12)
 index 3: n/a
 index 4: n/a
 */
+
+double ask_for_bet_amount(){
+  double players_bet;
+  cout << "How much would you like to bet? ";
+  cin >> players_bet;
+  if (players_bet_amount > players_bank){
+    cout << "Not enough funds!";
+    exit(1);
+  }
+  return players_bet;
+}
+
+void update_players_bets(double playr_bet_amount, double bet_idx){
+  for
+}
 
 // Bet Setting
 void setBet(int bet_index){
@@ -57,7 +75,9 @@ int roll_dice() {
 // Temp for printing bet arrays
 void print_bets(double array[NUM_BETS][2]){
   for (int i = 0; i < NUM_BETS; i++){
-    cout << array[i][0] << " ";
+    for (int j = 0; j < 2; j++){
+      cout << array[i][j] << " ";
+    }
   }
   cout <<"\n";
 }
@@ -83,19 +103,22 @@ void check_roll(double players_bets_arr[NUM_BETS][2], int players_dice_roll){
   }
 }
 
-void payout_bets_on_checked_roll(double checked_roll_arr[NUM_BETS][2], double players_bet_arr[NUM_BETS][2]){
+void payout_bets_on_checked_roll(double checked_roll_arr[NUM_BETS][2]){
   double players_payout{};
   if (checked_roll_arr[0][0] == 1){ //Basic Field Pays 1:1
     cout << "Player Wins Basic Field!" << endl;
-    players_payout = players_bet_arr[0][1] * 2.0;
+    players_payout = players_bets[0][1] * winning_bets[0][1];
+    players_bank = players_bank + players_payout;
   }
   if (checked_roll_arr[1][0] == 1){ //Two Field Pays 2:1
     cout << "Player Wins Two Field!" << endl;
-    players_payout = players_bet_arr[1][1] * 3.0;
+    players_payout = players_bets[1][1] * winning_bets[1][1];
+    players_bank = players_bank + players_payout;
   }
   if (checked_roll_arr[2][0] == 1){ //Twelve Field Pays 3:1
     cout << "Player Wins Twelve Field!" << endl;
-    players_payout = players_bet_arr[2][1] * 4.0;
+    players_payout = players_bets[2][1] * winning_bets[2][1];
+    players_bank = players_bank + players_payout;
   }
   if (checked_roll_arr[4][0] == 1){
 
@@ -110,20 +133,31 @@ void payout_bets_on_checked_roll(double checked_roll_arr[NUM_BETS][2], double pl
  /* MAIN FUNCTION BELOW */
 int main(){
   srand(time(NULL));
+  
   int players_roll = roll_dice();
   //int players_roll = 11;
-  cout << players_roll << endl;
-  check_roll(players_bets, players_roll);
-  print_bets(players_bets);
-  print_bets(winning_bets);
-  payout_bets_on_checked_roll(winning_bets, players_bets);
-
-  /*
-  std::cout << "\n***TEST**\n";
-  for(int j = 0; j < 20; j++){
-    print_bets(check_roll(players_bets, roll_dice()));
+  bool game_on = true;
+  //while (game_on){
+    players_bet_amount = ask_for_bet_amount();
+    update_players_bets(players_bet_amount);
+    cout << players_roll << endl;
+    check_roll(players_bets, players_roll);
+    print_bets(players_bets);
+    print_bets(winning_bets);
+    payout_bets_on_checked_roll(winning_bets);
+    cout << "Players Bank: " << players_bank << endl;
+   /* int x{};
+    cin >> x;
+    if (x == 1){
+      game_on = false;
+    }
+    srand(time(0));
   }
-  */
+*/
+
+
+
+  
 
 return 0;
 }
