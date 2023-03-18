666c754b18c3061518e21bebb425d6b2b4c8fe8c
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2023-03-18 18:55:45 +0000

    Update main

--- main.cpp
+++ main.cpp
@@ -27,7 +27,6 @@ double ask_for_bet_amount(){
   cout << "How much would you like to bet? ";
   cin >> players_bet;
 
-
   if (players_bet_amount > players_bank){
     cout << "Not enough funds!";
     exit(1);
