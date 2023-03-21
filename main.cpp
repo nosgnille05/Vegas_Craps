6368c7d383df49dd17a4e349a3806d57277a3f54
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2023-03-21 19:21:46 +0000

    Update main

--- main.cpp
+++ main.cpp
@@ -45,7 +45,6 @@ void clearBet(int bet_index){
 bool is_basic_field_roll(int dice_roll){
   if (dice_roll == 3 || dice_roll == 4 || dice_roll == 9 || dice_roll == 10 || dice_roll == 11){
 
-
     return true;
   }
   else return false;
